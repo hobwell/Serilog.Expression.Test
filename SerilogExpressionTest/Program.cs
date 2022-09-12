95f2da6719ff700c28a1e6265d14e5175d8dfc94
hobwell <11163233+hobwell@users.noreply.github.com>
2022-09-12 16:55:48 -0400

    removed some accidentally copied comments amended expression to look for "DROP" incase there is something special about "NULL"

--- SerilogExpressionTest/Program.cs
+++ SerilogExpressionTest/Program.cs
@@ -13,16 +13,13 @@ namespace SerilogExpressionTest {
         }
 
         /// <summary>
-        /// Overwrite the existing Logger with a new Logger configuration
+        /// Initialize the Logger with a new Logger configuration
         /// </summary>
         private static void SetLogger() {
-            //intialize the static logger using an enricher to populate the custom columns and event propeties
-            //note: this will only filter using the global list of exception filters.  Use the Init() method to add project level exception filtering
-            //note: the LevelSwitch seems to apply across all sinks regardless of where it is attached
             LoggerConfiguration config = new LoggerConfiguration()
             .Destructure.JsonNetTypes()
             .Enrich.With(new Enricher())
-            .WriteTo.Debug(new ExpressionTemplate("QueryString[?] like '%NULL%' ci: {#if QueryString[?] like '%NULL%' ci}True{#else}False{#end}\n\n"), restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, levelSwitch: null);
+            .WriteTo.Debug(new ExpressionTemplate("QueryString[?] like '%DROP%' ci: {#if QueryString[?] like '%DROP%' ci}True{#else}False{#end}\n\n"), restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, levelSwitch: null);
 
             Logger = config.CreateLogger();
         }
