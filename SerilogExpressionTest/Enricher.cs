d47365ba21f46ad2a51a850075db3c3f1b645f6f
hobwell <11163233+hobwell@users.noreply.github.com>
2022-09-12 16:44:59 -0400

    Add project files.

---
+++ SerilogExpressionTest/Enricher.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Serilog.Core;
+using Serilog.Events;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SerilogExpressionTest {
+    public class Enricher : ILogEventEnricher {
+
+        private static Dictionary<string, string> QueryString {
+            get {
+                return new Dictionary<string, string>() {
+                    { "x", "NULL" },
+                    { "y", "DROP" }
+                };
+            }
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory) {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("QueryString", QueryString, false));
+
+            Debug.WriteLine(JsonConvert.SerializeObject(logEvent));
+        }
+    }
+}
---
+++ SerilogExpressionTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Destructurama;
+using Serilog;
+using Serilog.Templates;
+
+namespace SerilogExpressionTest {
+    class Program {
+
+        private static Serilog.Core.Logger Logger;
+
+        static void Main(string[] args) {
+            SetLogger();
+            Logger.Error("Test");
+        }
+
+        /// <summary>
+        /// Overwrite the existing Logger with a new Logger configuration
+        /// </summary>
+        private static void SetLogger() {
+            //intialize the static logger using an enricher to populate the custom columns and event propeties
+            //note: this will only filter using the global list of exception filters.  Use the Init() method to add project level exception filtering
+            //note: the LevelSwitch seems to apply across all sinks regardless of where it is attached
+            LoggerConfiguration config = new LoggerConfiguration()
+            .Destructure.JsonNetTypes()
+            .Enrich.With(new Enricher())
+            .WriteTo.Debug(new ExpressionTemplate("QueryString[?] like '%NULL%' ci: {#if QueryString[?] like '%NULL%' ci}True{#else}False{#end}\n\n"), restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, levelSwitch: null);
+
+            Logger = config.CreateLogger();
+        }
+
+    }
+
+}
